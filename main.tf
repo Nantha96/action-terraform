b7d21d6620dcf7167a85503b80a68d62d71f5a5a
Nantha96 <nanthakmr2@gmail.com>
2021-04-13 15:43:43 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -30,22 +30,19 @@ provider "aws" {
 resource "aws_iam_role" "test_role" {
   name = "test_role"
 
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement" : [
-    {
-      "Effect" : "Allow",
-      "Action" : [
-        "ssm:DeleteDocument"
-      ],
-      "Resource" : [
-        "*"
-      ]
-    }
-  ]
-}
-EOF
+assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
 
   tags = {
       tag-key = "tag-value"
