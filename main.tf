8cb4063ef6ea48dcbc2bf5730db8e1a33afa3793
Nantha96 <nanthakmr2@gmail.com>
2021-04-13 15:50:12 +0530

    Updated policy

--- main.tf
+++ main.tf
@@ -34,12 +34,9 @@ assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
       {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Sid    = ""
-        Principal = {
-          Service = "ec2.amazonaws.com"
-        }
+        Action   = ["ec2:Describe*"]
+        Effect   = "Allow"
+        Resource = "*"
       },
     ]
   })
